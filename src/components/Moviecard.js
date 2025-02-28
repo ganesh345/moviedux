1a203a066593f83b38a69dacb26a40cf18716a9d
Ganesh Subrahmanyam Mathamsetti <ganesh@Ganeshs-MacBook-Air.local>
2025-02-28 11:49:23 +0530

    Added error handling features when image is not found

--- src/components/Moviecard.js
+++ src/components/Moviecard.js
@@ -2,9 +2,13 @@ import React from "react";
 import '../styles.css';
 
 export default function Moviecard({movie}) {
+    const ErrorHandling = (e) => {
+        e.target.src = 'images/default.png';
+    }
+
     return (
         <div className="movie-card" key={movie.id}>
-            <img src={`images/${movie.image}`} alt={movie.title} />
+            <img src={`images/${movie.image}`} alt={movie.title} onError = {ErrorHandling} />
             <div className="movie-card-info">
                 <h3 className="movie-card-title">{movie.title}</h3>
                 <p className="movie-card-genre">{movie.genre}</p>
