1152e3a94c8fd76739b60d2343ef3ea1903ef332
Ganesh Subrahmanyam Mathamsetti <ganesh@Ganeshs-MacBook-Air.local>
2025-02-28 18:21:02 +0530

    removed movies from the component and added to app.js for better access to the application

--- src/components/Moviesgrid.js
+++ src/components/Moviesgrid.js
@@ -1,19 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../styles.css";
 import Moviecard from "./Moviecard";
 
-export default function Moviesgrid() {
-  const [movies, setmovies] = useState([]);
+export default function Moviesgrid({movies}) {
+  
   const [search, setSearch] = useState("");
 
   const [genre, setGenre] = useState("All Genres");
   const [rating, setRating] = useState("All");
-
-  useEffect(() => {
-    fetch("/movies.json")
-      .then((response) => response.json())
-      .then((data) => setmovies(data));
-  }, []);
 
   const HandleSearchChange = (e) => {
     setSearch(e.target.value);
