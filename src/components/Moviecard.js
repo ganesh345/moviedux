4c8b915a061a05431ecca736c8062f948f700b14
Ganesh Subrahmanyam Mathamsetti <ganesh@Ganeshs-MacBook-Air.local>
2025-02-28 19:41:08 +0530

    added toogle slider to the functionality

--- src/components/Moviecard.js
+++ src/components/Moviecard.js
@@ -1,29 +1,49 @@
 import React from "react";
-import '../styles.css';
+import "../styles.css";
 
-export default function Moviecard({movie}) {
-    const ErrorHandling = (e) => {
-        e.target.src = "images/default.jpg";
-    };
+export default function Moviecard({ movie, isWatchlisted, toogleWatchlist }) {
+  const ErrorHandling = (e) => {
+    e.target.src = "images/default.jpg";
+  };
 
-    const getRatingClass = (rating) => {
-        if (rating >= 8) {
-            return 'rating-good';
-        } else if (rating >= 5 && rating < 8) {
-            return 'rating-ok';
-        } else {
-            return 'rating-bad';
-        }
-    };
+  const getRatingClass = (rating) => {
+    if (rating >= 8) {
+      return "rating-good";
+    } else if (rating >= 5 && rating < 8) {
+      return "rating-ok";
+    } else {
+      return "rating-bad";
+    }
+  };
 
-    return (
-        <div className="movie-card" key={movie.id}>
-            <img src={`images/${movie.image}`} alt={movie.title} onError = {ErrorHandling} />
-            <div className="movie-card-info">
-                <h3 className="movie-card-title">{movie.title}</h3>
-                <p className="movie-card-genre">{movie.genre}</p>
-                <p className={`movie-card-rating ${getRatingClass(movie.rating)}`}>{movie.rating}</p>
-            </div>
+  return (
+    <div className="movie-card" key={movie.id}>
+      <img
+        src={`images/${movie.image}`}
+        alt={movie.title}
+        onError={ErrorHandling}
+      />
+      <div className="movie-card-info">
+        <h3 className="movie-card-title">{movie.title}</h3>
+        <div>
+          <span className="movie-card-genre">{movie.genre}</span>
+          <span className={`movie-card-rating ${getRatingClass(movie.rating)}`}>
+            {movie.rating}
+          </span>
         </div>
-    );
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={isWatchlisted}
+            onChange={() => toogleWatchlist(movie.id)}
+          />
+          <span className="slider">
+            <span className="slider-label">
+              {isWatchlisted ? "In Watchlist" : "Add to Watchlist"}
+            </span>
+          </span>
+        </label>
+      </div>
+    </div>
+  );
 }
