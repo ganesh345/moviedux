5372f8adc083b3fe7ac64e4104392ca62b56f2da
Ganesh Subrahmanyam Mathamsetti <ganesh@Ganeshs-MacBook-Air.local>
2025-02-28 13:04:41 +0530

    Added search bar functionality to the Moviegrid component

--- src/components/Moviesgrid.js
+++ src/components/Moviesgrid.js
@@ -4,18 +4,40 @@ import Moviecard from "./Moviecard";
 
 export default function Moviesgrid() {
   const [movies, setmovies] = useState([]);
+  const [search, setSearch] = useState([]);
 
   useEffect(() => {
     fetch("movies.json")
       .then((response) => response.json())
       .then((data) => setmovies(data));
   }, []);
+
+  const HandleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredMovies = (movies || []).filter((movie) => 
+  movie.title?.toLowerCase().includes(String(search || "").toLowerCase())
+);
+
+  
   return (
-    <div className="movies-grid">                          { /* create a div with the class name movies-grid*/}
-      {movies.map((movie) => (
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search by movie name"
+        value = {search}
+        onChange = {HandleSearchChange}
+        />
+      <div className="movies-grid">                          { /* create a div with the class name movies-grid*/}
+      {filteredMovies.map((movie) => (
         <Moviecard key={movie.id} movie={movie}> { /*pass the movie object to the Moviecard component*/ }
         </Moviecard> 
       ))}
     </div>
+
+    </div>
+    
   );
 }
