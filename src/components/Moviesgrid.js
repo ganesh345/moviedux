99c192744487e44d8a09c0a218ab731880e2613c
Ganesh Subrahmanyam Mathamsetti <ganesh@Ganeshs-MacBook-Air.local>
2025-02-28 11:30:01 +0530

    created the Moviescard component and pass the props to moviesgrid coponent

--- src/components/Moviesgrid.js
+++ src/components/Moviesgrid.js
@@ -1,29 +1,21 @@
-import React, {useState, useEffect} from "react";
-import '../styles.css';
+import React, { useState, useEffect } from "react";
+import "../styles.css";
+import Moviecard from "./Moviecard";
 
 export default function Moviesgrid() {
-    const [movies, setmovies] = useState([]);
+  const [movies, setmovies] = useState([]);
 
-    useEffect(() => {
-        fetch("movies.json")
-        .then((response) => response.json())
-        .then(data =>setmovies(data));
-    }, []);
-    return (
-        <div className="movies-grid">
-            {
-                movies.map((movie) => (
-                    <div className="movie-card" key={movie.id}>
-                        <img src={`images/${movie.image}`} alt={movie.title} />
-                        <div className="movie-card-info">
-                        <h3 className="movie-card-title">{movie.title}</h3>
-                        <p className="movie-card-genre">{movie.genre}</p>
-                        <p className="movie-card-rating">{movie.rating}</p>
-                        </div>
-                    </div>
-                ))
-            }
-
-        </div>
-    )
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setmovies(data));
+  }, []);
+  return (
+    <div className="movies-grid">                          { /* create a div with the class name movies-grid*/}
+      {movies.map((movie) => (
+        <Moviecard key={movie.id} movie={movie}> { /*pass the movie object to the Moviecard component*/ }
+        </Moviecard> 
+      ))}
+    </div>
+  );
 }
