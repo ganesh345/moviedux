9b9716e246c751de2e4ca48b8a531264197f6e7e
Ganesh Subrahmanyam Mathamsetti <ganesh@Ganeshs-MacBook-Air.local>
2025-02-28 18:23:03 +0530

    added movies to the app.js for better access because it is the home page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,37 @@ import './styles.css'; // Importing the styles.css file
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Moviesgrid from './components/Moviesgrid';
+import Watchlist from './components/Watchlist';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [movies, setmovies] = useState([]);
+
+  useEffect(() => {
+    fetch("/movies.json")
+      .then((response) => response.json())
+      .then((data) => setmovies(data));
+  }, []);
+
   return (
     <div className="App">
       <div className='container'>
         <Header></Header>
-        <Moviesgrid></Moviesgrid>
+        <Router>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/watchlist">Watchlist</Link></li>
+            </ul> 
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<Moviesgrid movies={movies} />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+          </Routes>
+        </Router>
       </div>
     
     <Footer></Footer>
