fc6733dcf1102705087a014ed22aa1fde2ef181c
Ganesh Subrahmanyam Mathamsetti <ganesh@Ganeshs-MacBook-Air.local>
2025-02-28 18:19:45 +0530

    Added Watchlist feature to the application

---
+++ src/components/Watchlist.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles.css";
+
+export default function Watchlist() {
+  return (
+    <div>
+      <h1 className="title"> your Watchlist</h1>
+    </div>
+  );
+}
