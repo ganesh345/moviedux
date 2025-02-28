0a569786214a4800533cd7cadf9d656f9dc54b29
Ganesh Subrahmanyam Mathamsetti <ganesh@Ganeshs-MacBook-Air.local>
2025-02-28 19:44:09 +0530

    Added watchlist feature to the component to check the watchlisted movies

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "../styles.css";
+import Moviecard from "./Moviecard";
 
-export default function Watchlist() {
+export default function Watchlist({ movies, watchlist, toggleWatchlist }) {
   return (
     <div>
-      <h1 className="title"> your Watchlist</h1>
+      <h1 className="title">Your Watchlist</h1>
+      <div className="watchlist">
+        {watchlist.map((id) => {
+          const movie = movies.find((movie) => movie.id === id);
+          return (
+            <Moviecard
+              key={movie.id}
+              movie={movie}
+              isWatchlisted={true}
+              toggleWatchlist={toggleWatchlist}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
